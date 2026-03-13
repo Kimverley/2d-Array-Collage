3b9b764f5521d8c78814795b0b88ae2a1ce6e645
Kimverley <kimy2022hm@icloud.com>
2026-03-12 17:16:33 -0700

    Add files via upload

---
+++ collage.js
@@ -0,0 +1,26 @@
+//table border 1
+
+let image =[
+    [ "monkey1","monkey2","monkey3"],
+    [ "monkey4","monkey5","monkey6"],
+    [ "monkey7","monkey8","monkey9"],  
+]
+
+let collage = document.getElementById('collage');
+let html = "<table border=1>";
+//one loop to go through the rows, and another loop to go through the items inside each row
+for (let i = 0; i < image.length; i++)
+    
+ {//nested loop
+
+//table row
+
+    html+="<tr>";
+  for (let j = 0; j < image[i].length; j++) {
+    html+=`<td> <img src="/images/${image[i][j]}.png"></td>`;
+   
+  }
+  html+="<tr>";
+}
+html+="</table>";
+collage.innerHTML = html;
